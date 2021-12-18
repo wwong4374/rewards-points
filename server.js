c14dddca44e65e249306e170747a316d5fe52551
Wilson Wong <wilson.ka.wong@gmail.com>
2021-12-18 11:58:44 -0600

    Fixed a screenshot

--- server.js
+++ server.js
@@ -77,8 +77,9 @@ const spendPoints = (pointsToSpend) => {
 // API ENDPOINTS:
 // POINTS SPEND
 app.get('/points/spend', (req, res) => {
-  const { pointsToSpend } = req.query;
-  const spentPoints = spendPoints(pointsToSpend);
+  console.log(req.query);
+  const { points } = req.query;
+  const spentPoints = spendPoints(points);
   res.send(spentPoints);
 });
 
